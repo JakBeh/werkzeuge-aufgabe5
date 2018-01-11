79a68ad94e6f225327b5aa45ee5fabb3b71cc6fc
Jakob Behnke <jakob.behnke@gmx.de>
2018-01-12 00:33:38 +0100

    Added README.m and fixed merge to desired output

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
     public static void main (String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World");
-=======
-        System.out.println("Hallo Welt");
->>>>>>> bugfix
+        System.out.println("Moin Moin");
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+### Benutzen der Dateien  
+Benutzen Sie zuerst folgenden Befehl:  
+> javac HelloWorld.java  
+
+Benutzen Sie danach folgenden Befehl:  
+> java HelloWorld  
+
